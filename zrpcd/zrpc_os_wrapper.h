12c619aa1538ee89d6a7b5dfc14cd09f3be82a72
Philippe Guibert <philippe.guibert@6wind.com>
2016-11-18 19:00:43 +0100

    zrpc: os wrapper header introduction
    This header is here to handle basic type declarations that are not
    defined by system.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

---
+++ zrpcd/zrpc_os_wrapper.h
@@ -0,0 +1,28 @@
+/* zrpc  OS wrapper
+ * Copyright (c) 2016 6WIND,
+ *
+ * This file is part of GNU Quagga.
+ *
+ * GNU Zebra is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * GNU Quagga is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Quagga; see the file COPYING.  If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+#ifndef _ZRPC_OS_WRAPPER_H_ /* _ZRPC_OS_WRAPPER_H_ */
+#define _ZRPC_OS_WRAPPER_H_
+
+#include <stdint.h>
+
+typedef unsigned short uint16_t;
+
+#endif /* _ZRPC_OS_WRAPPER_H_ */
