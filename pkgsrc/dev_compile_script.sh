56b91af1bf521d44872cf0fe42ad7308fb63ab14
maheshloni <mahesh.loni@gmail.com>
2017-07-25 21:27:56 +0530

    Update dev_compile_script.sh
    Philippe , 
    Updated your review comments

--- pkgsrc/dev_compile_script.sh
+++ pkgsrc/dev_compile_script.sh
@@ -1,4 +1,4 @@
-# Copyright 2016 Ericsson
+# Copyright 2016 Tata Consulting and Ericsson
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
