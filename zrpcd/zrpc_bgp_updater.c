28c010067941beb7c42c3f3f70f0771837a875bf
Philippe Guibert <philippe.guibert@6wind.com>
2019-01-28 11:38:11 +0100

    zrpcd: handle retry for peerUp() and peerDown() events
    those peerUp() and peerDown() events are handled.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- zrpcd/zrpc_bgp_updater.c
+++ zrpcd/zrpc_bgp_updater.c
@@ -305,34 +305,27 @@ zrpc_bgp_updater_on_notification_send_event (const gchar * prefix, const gint8 e
 gboolean
 zrpc_bgp_updater_peer_up (const gchar * ipAddress, const gint64 asNumber)
 {
-  GError *error = NULL;
+  GError *error = NULL, **perror;
   gboolean response;
   struct zrpc_vpnservice *ctxt = NULL;
+  int thrift_tries;
 
+  perror = &error;
   zrpc_vpnservice_get_context (&ctxt);
   if(!ctxt || !ctxt->bgp_updater_client)
       return FALSE;
 
-  response = bgp_updater_client_peer_up(ctxt->bgp_updater_client,
-                                        ipAddress, asNumber, &error);
-   if (error != NULL)
-    {
-      if (error->domain == THRIFT_TRANSPORT_ERROR &&
-          error->code == THRIFT_TRANSPORT_ERROR_SEND)
-        {
-          ctxt->bgp_update_thrift_lost_msgs++;
-          zrpc_info ("peerUp(): sent error %s", error->message);
-          response = FALSE;
-          zrpc_transport_cancel_monitor(ctxt);
-          zrpc_transport_check_response(ctxt, response);
-        }
-      g_clear_error (&error);
-      error = NULL;
-    }
-
+  for (thrift_tries = 0; thrift_tries < 2; thrift_tries++) {
+    response = bgp_updater_client_peer_up(ctxt->bgp_updater_client,
+                                          ipAddress, asNumber, &error);
+    if (zrpc_bgp_updater_handle_response(ctxt, (bool *)&response, perror, "peerUp()") == FALSE)
+      break;
+    error = NULL;
+  }
  if(IS_ZRPC_DEBUG_NOTIFICATION)
-    zrpc_log ("peerUp(%s, asNumber %u) %s", \
-              ipAddress, asNumber, response == FALSE?"NOK":"OK");
+    zrpc_log ("peerUp(%s, asNumber %u) %s",
+              ipAddress, asNumber,
+              response == FALSE?"NOK":"OK");
   return response;
 }
 
@@ -342,34 +335,27 @@ zrpc_bgp_updater_peer_up (const gchar * ipAddress, const gint64 asNumber)
 gboolean
 zrpc_bgp_updater_peer_down (const gchar * ipAddress, const gint64 asNumber)
 {
-  GError *error = NULL;
+  GError *error = NULL, **perror;
   gboolean response;
   struct zrpc_vpnservice *ctxt = NULL;
+  int thrift_tries;
 
+  perror = &error;
   zrpc_vpnservice_get_context (&ctxt);
   if(!ctxt || !ctxt->bgp_updater_client)
       return FALSE;
 
-  response = bgp_updater_client_peer_down(ctxt->bgp_updater_client,
-                                          ipAddress, asNumber, &error);
-   if (error != NULL)
-    {
-      if (error->domain == THRIFT_TRANSPORT_ERROR &&
-          error->code == THRIFT_TRANSPORT_ERROR_SEND)
-        {
-          ctxt->bgp_update_thrift_lost_msgs++;
-          zrpc_info ("peerDown(): sent error %s", error->message);
-          response = FALSE;
-          zrpc_transport_cancel_monitor(ctxt);
-          zrpc_transport_check_response(ctxt, response);
-        }
-      g_clear_error (&error);
-      error = NULL;
-    }
-
+  for (thrift_tries = 0; thrift_tries < 2; thrift_tries++) {
+    response = bgp_updater_client_peer_down(ctxt->bgp_updater_client,
+                                            ipAddress, asNumber, &error);
+    if (zrpc_bgp_updater_handle_response(ctxt, (bool *)&response, perror, "peerDown()") == FALSE)
+      break;
+    error = NULL;
+  }
  if(IS_ZRPC_DEBUG_NOTIFICATION)
-    zrpc_log ("peerDown(%s, asNumber %u) %s", \
-              ipAddress, asNumber, response == FALSE?"NOK":"OK");
+    zrpc_log ("peerDown(%s, asNumber %u) %s",
+              ipAddress, asNumber,
+              response == FALSE?"NOK":"OK");
   return response;
 }
 #endif
