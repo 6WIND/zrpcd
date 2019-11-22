41c9e19d89de3382786241293a6201a0c7df1ead
Philippe Guibert <philippe.guibert@6wind.com>
2019-11-22 11:37:54 +0100

    Revert "zrpcd: force tcp packet send out in queued"
    This reverts commit a5f874276a577887423e730ec51be638752d994b.

--- zrpcd/zrpc_bgp_updater.c
+++ zrpcd/zrpc_bgp_updater.c
@@ -98,20 +98,6 @@ static bool zrpc_bgp_updater_handle_response(struct zrpc_vpnservice *ctxt,
     return should_retry;
 }
 
-/* each time force tcp packet send out immediately in queued */
-static int zrpc_flush_socket(int tcp_sock)
-{
-  int val = 1;
-  int ret = 0;
-
-  if (setsockopt (tcp_sock, IPPROTO_TCP, TCP_NODELAY, (char *) &val, sizeof (val)) < 0)
-  {
-    zrpc_info("%s fd=%d TCP_NODELAY failed error=%d\n", __func__, tcp_sock, errno);
-    ret = -1;
-  }
-  return ret;
-}
-
 /*
  * update push route notification message
  * sent when a vpnv4 route is pushed
@@ -157,7 +143,7 @@ zrpc_bgp_updater_on_update_push_route (const protocol_type p_type, const gchar *
       break;
     error = NULL;
   }
-  zrpc_flush_socket(zrpc_vpnservice_get_bgp_updater_socket (ctxt));
+
   if(IS_ZRPC_DEBUG_NOTIFICATION)
   {
     char ethtag_str[20];
@@ -216,7 +202,6 @@ zrpc_bgp_updater_on_update_withdraw_route (const protocol_type p_type, const gch
       break;
     error = NULL;
   }
-  zrpc_flush_socket(zrpc_vpnservice_get_bgp_updater_socket (ctxt));
   if(IS_ZRPC_DEBUG_NOTIFICATION)
     {
       char ethtag_str[20];
@@ -246,7 +231,6 @@ zrpc_bgp_updater_on_start_config_resync_notification_quick (struct zrpc_vpnservi
       break;
     error = NULL;
   }
-  zrpc_flush_socket(zrpc_vpnservice_get_bgp_updater_socket (ctxt));
   if(IS_ZRPC_DEBUG_NOTIFICATION)
     zrpc_info ("onStartConfigResyncNotification() %s", response == FALSE?"NOK":"OK");
   return response;
@@ -308,7 +292,6 @@ zrpc_bgp_updater_on_notification_send_event (const gchar * prefix, const gint8 e
       break;
     error = NULL;
   }
- zrpc_flush_socket(zrpc_vpnservice_get_bgp_updater_socket (ctxt));
  if(IS_ZRPC_DEBUG_NOTIFICATION)
     zrpc_log ("onNotificationSendEvent(%s, errCode %d, errSubCode %d) %s", \
                prefix, errCode, errSubcode, response == FALSE?"NOK":"OK");
@@ -339,7 +322,6 @@ zrpc_bgp_updater_peer_up (const gchar * ipAddress, const gint64 asNumber)
       break;
     error = NULL;
   }
- zrpc_flush_socket(zrpc_vpnservice_get_bgp_updater_socket (ctxt));
  if(IS_ZRPC_DEBUG_NOTIFICATION)
     zrpc_log ("peerUp(%s, asNumber %u) %s",
               ipAddress, asNumber,
@@ -370,7 +352,6 @@ zrpc_bgp_updater_peer_down (const gchar * ipAddress, const gint64 asNumber)
       break;
     error = NULL;
   }
- zrpc_flush_socket(zrpc_vpnservice_get_bgp_updater_socket (ctxt));
  if(IS_ZRPC_DEBUG_NOTIFICATION)
     zrpc_log ("peerDown(%s, asNumber %u) %s",
               ipAddress, asNumber,
